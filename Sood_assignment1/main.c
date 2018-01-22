2f33c54e447c82784fb277d5b509ef3b250e8014
Rishu <ritesh324@icloud.com>
2018-01-21 22:59:17 -0700

    updated with comments

--- Sood_assignment1/main.c
+++ Sood_assignment1/main.c
@@ -2,14 +2,26 @@
 //  main.c
 //  OS_Assign1
 //
-//  Created by Ritesh Sood on 1/21/18.
-//  Copyright © 2018 Ritesh Sood. All rights reserved.
-//
+//  The program reads in words from a file,
+//  stores them into a binary tree. It keeps
+//  track of how many times each word appeared
+//  in the file. It writes all of the words to
+//  an output file in alphabetical order. Next
+//  to the words it displays the number of times
+//  they appeared.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+////////////////////////////////////////////////////
+// The node struct represnts what each node on    //
+// the binary tree will look like. It has a char  //
+// string to hold the word we are inserting. An   //
+// int count to track the amount of times that    //
+// word appears. Two left and right Node pointers //
+// to point to subtree nodes.                     //
+////////////////////////////////////////////////////
 typedef struct Node {
 
     char *word;
@@ -19,7 +31,14 @@ typedef struct Node {
     
 } Node;
 
-
+////////////////////////////////////////////////////////
+// inOrderInsert does an in-order recursive traversal //
+// through the binary tree and inserts the word.      //
+// If the word already has a node on the tree, the    //
+// function increments the count for that node.       //
+// It also frees the node which was allocated in      //
+// insertWord, if it already exists.                  //
+////////////////////////////////////////////////////////
 void inOrderInsert(Node* leaf, Node* str)
 {
     int check = strcmp(leaf->word, str->word);
@@ -45,6 +64,12 @@ void inOrderInsert(Node* leaf, Node* str)
     }
 }
 
+/////////////////////////////////////////
+// insertWord allocates a new node     //
+// using malloc and then call on       //
+// inOrderInsert and passes in the new //
+// node to be stored onto the tree.    //
+/////////////////////////////////////////
 void insertWord(Node *root, char* str){
     
     Node* newNode = (struct Node *) malloc(sizeof(struct Node));
@@ -63,6 +88,30 @@ void insertWord(Node *root, char* str){
         inOrderInsert(root, newNode);
 }
 
+////////////////////////////////////////////
+// inOrderFree does an inorder traversal  //
+// throught the tree and frees every node //
+// from memory.                           //
+////////////////////////////////////////////
+void inOrderFree(Node *leaf){
+    
+    if(leaf->left != NULL)
+        inOrderFree(leaf->left);
+    
+    free(leaf->word);
+    free(leaf);
+    
+    if(leaf->right != NULL)
+        inOrderFree(leaf->right);
+    
+}
+
+//////////////////////////////////////////////
+// inOrderPrint does an inorder traversal   //
+// through the tree and writes the contents //
+// of each node to an output file.          //
+//////////////////////////////////////////////
+
 void inOrderPrint(Node *leaf, FILE *oFile){
     
     if(leaf->left != NULL)
@@ -72,16 +121,22 @@ void inOrderPrint(Node *leaf, FILE *oFile){
     
     if(leaf->right != NULL)
         inOrderPrint(leaf->right, oFile);
-        
+    
 }
 
-
+//////////////////////////////////////////////
+// main opens a file to read from and a     //
+// file to write to. This is where memory   //
+// is allocated for a root node. A do while //
+// loop steps through the file and inserts  //
+// each word into the tree.                 //
+//////////////////////////////////////////////
 int main(int argc, char **argv) {
     
     FILE *fptr;
     FILE *oFptr;
-    char fileName[] = "input02.txt";
-    char outFile[] = "output01.txt";
+    char fileName[] = "/Users/riteshsood/Desktop/os/input02.txt";
+    char outFile[] = "/Users/riteshsood/Desktop/os/output01.txt";
     fptr = fopen(fileName, "r");
     oFptr = fopen(outFile, "w");
     
@@ -105,8 +160,10 @@ int main(int argc, char **argv) {
     fclose(fptr);
     
     inOrderPrint(root, oFptr);
+    inOrderFree(root);
     
     fclose(oFptr);
     
     exit(0);
 }
+
